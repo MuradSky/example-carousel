dfb82b483a0f8441af7a70ed0c4ca6bded271c05
Murad <skytechnic.music@gmail.com>
2024-06-04 12:41:13 +0300

    update

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,22 +1,14 @@
 import {BrowserRouter} from 'react-router-dom';
 import {LayoutStyles} from './LayoutStyles';
-// import {Header} from './components/Header';
-// import {Footer} from './components/Footer';
 import {Router} from './Router';
-import {EmailConfirm, NoGiveaway} from '../components';
 import {useRefreshToken} from 'helpers/hooks';
 
 export const Layout = () => {
 	useRefreshToken()
-
 	return (
 		<LayoutStyles>
 			<BrowserRouter>
-				{/*<Header />*/}
 				<Router />
-				{/*<Footer />*/}
-				<EmailConfirm />
-				<NoGiveaway/>
 			</BrowserRouter>
 		</LayoutStyles>
 	)
